b84948673e6eac129b531875d0c19c627c31c6ee
Jenwng <Jenniescwong@gmail.com>
2019-12-20 13:04:18 -0500

    deleted

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -28,20 +28,9 @@ export default class Portfolio extends Component {
                       </div>
                       <div className="Portfolio-label">
                         <h2>
-                          Internship at Juniper <span>2019-present</span>
+                          <span></span>
                         </h2>
-                        <p>
-                          I recently have joined the Juniper Networs as a
-                          Software Developer-Intern in the DevOps team. My major
-                          part of the work has been into the field of creating
-                          sustainable and flexible CI/CD (Continuous Integration
-                          & Continuous Development) pipelines which enables
-                          developers at Juniper to do rapid development. I am
-                          also exploring the role of container technology like
-                          Docker and container-orchrastation technologies like
-                          K8s, Swarm to create industry standard production
-                          environments.
-                        </p>
+                        <p></p>
                       </div>
                     </div>
                   </article>
@@ -55,17 +44,9 @@ export default class Portfolio extends Component {
                       </div>
                       <div className="Portfolio-label">
                         <h2>
-                          Undergraduation at DAIICT<span>2015-2019</span>
+                          <span></span>
                         </h2>
-                        <p>
-                          I am pursuing my under-graduation studies with major
-                          in I.C.T.(Information & Communication Technology). I
-                          have taken courses like DSA, OOPs, Computer Networks,
-                          IOT, Web-Data Management over the years and have
-                          better understanding of these subjects.I have also
-                          been part of S.B.G.(Student Body Govenance) scince my
-                          first year of college.
-                        </p>
+                        <p></p>
                       </div>
                     </div>
                   </article>
@@ -79,17 +60,9 @@ export default class Portfolio extends Component {
                       </div>
                       <div className="Portfolio-label">
                         <h2>
-                          Primary and Higher Education <span>2003-2015</span>
+                          <span></span>
                         </h2>
-                        <p>
-                          I have completed my higher secondary education with
-                          major subjects as Physics,Chemistry & Maths with 91
-                          merit. During my time at school, I have developed
-                          interest in solving complex problems of the
-                          fundamental physics which helped me to improve my
-                          understanding of any problem and also my mathematical
-                          skills to actually solve the problems.
-                        </p>
+                        <p></p>
                       </div>
                     </div>
                   </article>
